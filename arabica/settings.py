8de7706932e9735938134083ee66bfc05b9a3c84
ourfalia <ourfalia@tcd.ie>
2022-11-20 20:46:57 +0000

    fixing indentation

--- arabica/settings.py
+++ arabica/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'arabica.wsgi.application'
 
 DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
