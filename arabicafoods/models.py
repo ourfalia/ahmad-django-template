f5a9e16efef56df2bd51346c227b9acc6deaa025
ourfalia <ourfalia@tcd.ie>
2022-11-24 10:26:13 +0000

    add content to reservation page

--- arabicafoods/models.py
+++ arabicafoods/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
 
 # Create your models here.
+class Item(models.Model):
+    number = models.CharField(max_length=50, null=False, blank=False)
+    booked = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.number
--- arabicafoods/views.py
+++ arabicafoods/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def get_home(request):
     return render(request, '../templates/home/home.html')
 
 def make_reservation(request):
-    return render(request, '../templates/home/reservation.html')
+    items = Item.objects.all
+    context = {
+        'items' : items
+    }
+    return render(request, '../templates/home/reservation.html', context)
 
 def go_to_menu(request):
     return render(request, '../templates/home/menu.html')
